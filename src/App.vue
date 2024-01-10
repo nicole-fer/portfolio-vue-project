687d1b280908c1549155d5fee670ab7904d542fe
nicole-fer <nicolefernandes@alu.ufc.br>
2024-01-10 12:12:29 -0300

    Inicinado portifolio com vuejs e adicionando taiwlnd...

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+</script>
+
+<template>
+  <div>
+      <div>
+        <Header />
+      </div>
+    <main>
+      <Main />
+    </main>
+  </div>
+</template>
+
+
+
+

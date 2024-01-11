a0d9a53ea2985ac338c445464077141a04456662
nicole-fer <nicolefernandes@alu.ufc.br>
2024-01-11 15:23:00 -0300

    iniciando parte de contao

--- src/components/Conteudo/Contato.vue
+++ src/components/Conteudo/Contato.vue
@@ -1,3 +1,41 @@
+<script setup>
+const props = defineProps({
+    darkMode: Boolean,
+});
+</script>
+
 <template>
-    <div class="my-64">Contato</div>
+    <div :class="darkMode == true
+            ? 'pt-12 bg-gray-800 flex justify-center'
+            : 'pt-12 bg-pink-100 flex justify-center'
+        ">
+        <div class="sm:w-1/2">
+            <div class="text-center h-10/12">
+                <p :class="darkMode == true
+                        ? 'text-pink-500 font-mono text-lg font-medium'
+                        : 'text-pink-600 font-mono text-lg  font-medium'
+                    ">
+                    Se interessou?
+                </p>
+                <p :class="darkMode == true
+                        ? 'text-5xl text-gray-200 mt-2 font-semibold'
+                        : 'font-medium text-5xl text-gray-700 mt-2'
+                    ">
+                    Entre em Contato
+                </p>
+                <p>
+                    Caso queira entrar em contato, conecte-se comigo nas redes sociais. Estou pronta para criar novas experiências!"
+                </p>
+            </div>
+            <div :class="darkMode == true
+                    ? 'text-white text-sm font-mono pb-4 h-2/12'
+                    : 'text-center text-sm font-mono pb-4'
+                ">
+                <div class="text-center">
+                    <p class="self-end">Desenvolvido por Nicole Fernandes</p>
+                    <p class="self-end">2024</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
--- src/components/Conteudo/Sobre.vue
+++ src/components/Conteudo/Sobre.vue
@@ -7,9 +7,9 @@ const props = defineProps({
 <template>
     <div :class="darkMode == true ? 'grid grid-cols-5  text-white sm:h-screen py-10 sm:py-0 bg-gradient-to-b from-gray-800 to-cyan-800' : 'bg-gradient-to-b from-pink-100 to-pink-200  grid grid-cols-5 h-screen'">
         <div class="sm:px-8	flex place-items-center sm:col-span-3 col-span-5 sm:mr-12 px-4">
-          <div class="sm:-mt-64">
+          <div class="sm:-mt-32">
             <p :class=" darkMode==true  ? 'sm:text-7xl text-4xl font-mono font-medium text-pink-400' : 'sm:text-7xl text-4xl font-mono font-medium text-pink-600'">Olá! Me chamo Nicole Fernandes</p>
-            <p :class="darkMode==true ? 'sm:text-4xl text-xl text-justify text-pink-200 pr-3': 'sm:text-4xl text-xl text-justify text-blue-950'">Sou desenvolvedora web e graduada em Engenharia da Computação pela Universidade Federal do Ceará (UFC). Seja bem vindo(a) ao meu portifólio.</p>
+            <p :class="darkMode==true ? 'sm:text-2xl text-xl text-justify text-pink-200 pr-3 mt-5': 'sm:text-2xl mt-5 text-xl text-justify text-blue-950'">Sou desenvolvedora web e graduada em Engenharia da Computação pela Universidade Federal do Ceará (UFC). Seja bem vindo(a) ao meu portifólio.</p>
           </div>
         </div>
         <div class="sm:-mt-12 place-items-center sm:col-span-2 flex justify-end sm:flex hidden">

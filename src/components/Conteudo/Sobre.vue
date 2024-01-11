246a5ed4b912ed15747c73699f251c38098c21c8
nicole-fer <nicolefernandes@alu.ufc.br>
2024-01-11 14:27:45 -0300

    toranando pag princial responsiva

--- src/components/Conteudo/Sobre.vue
+++ src/components/Conteudo/Sobre.vue
@@ -5,14 +5,14 @@ const props = defineProps({
 </script>
 
 <template>
-    <div :class="darkMode == true ? 'grid grid-cols-5  text-white h-screen bg-gradient-to-b from-gray-800 to-cyan-800' : 'bg-gradient-to-b from-pink-100 to-pink-200  grid grid-cols-5 h-screen'">
-        <div class="sm:px-8	flex place-items-center col-span-3 sm:mr-12">
+    <div :class="darkMode == true ? 'grid grid-cols-5  text-white sm:h-screen py-10 sm:py-0 bg-gradient-to-b from-gray-800 to-cyan-800' : 'bg-gradient-to-b from-pink-100 to-pink-200  grid grid-cols-5 h-screen'">
+        <div class="sm:px-8	flex place-items-center sm:col-span-3 col-span-5 sm:mr-12 px-4">
           <div class="sm:-mt-64">
-            <p :class=" darkMode==true  ? 'text-7xl font-mono font-medium text-pink-400' : 'text-7xl font-mono font-medium text-pink-600'">Olá! Me chamo Nicole Fernandes</p>
-            <p :class="darkMode==true ? 'text-4xl text-justify text-pink-200': 'text-4xl text-justify text-blue-950'">Sou desenvolvedora web e graduada em Engenharia da Computação pela Universidade Federal do Ceará (UFC). Seja bem vindo(a) ao meu portifólio.</p>
+            <p :class=" darkMode==true  ? 'sm:text-7xl text-4xl font-mono font-medium text-pink-400' : 'sm:text-7xl text-4xl font-mono font-medium text-pink-600'">Olá! Me chamo Nicole Fernandes</p>
+            <p :class="darkMode==true ? 'sm:text-4xl text-xl text-justify text-pink-200 pr-3': 'sm:text-4xl text-xl text-justify text-blue-950'">Sou desenvolvedora web e graduada em Engenharia da Computação pela Universidade Federal do Ceará (UFC). Seja bem vindo(a) ao meu portifólio.</p>
           </div>
         </div>
-        <div class="sm:-mt-12 place-items-center col-span-2 flex justify-end">
+        <div class="sm:-mt-12 place-items-center sm:col-span-2 flex justify-end sm:flex hidden">
             <img class="h-2/3 w-3/5" src="../../assets/img/icon.png"/>
         </div>
     </div>

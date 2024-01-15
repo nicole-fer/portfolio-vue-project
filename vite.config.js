b605a227559fbf81eaf1827e0e4cbbc2a6c68963
nicole-fer <nicolefernandes@alu.ufc.br>
2024-01-15 14:07:50 -0300

    change path

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/portfolio-vue-project/',
   plugins: [
     vue(),
   ],
+  base:'/portfolio-vue-project',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
